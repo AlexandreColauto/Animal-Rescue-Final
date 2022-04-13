6de5e765f2f81fb7c84feec97bec40f065d7f33d
Alexandre Colauto <43041764+AlexandreColauto@users.noreply.github.com>
2022-04-13 03:28:01 +0100

    Searching_Not_Ready

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -10,6 +10,7 @@ import Link from "next/link";
 import ToastSucess from "../src/components/ToastSucess";
 import ToastError from "../src/components/ToastError";
 import Processing from "../src/components/Processing";
+import Search from "../src/components/Search";
 
 interface marketItms {
   collectionAddress: string;
@@ -45,6 +46,10 @@ function Explore() {
   const [isSuccess, setIsSuccess] = useState(false);
   const [isError, setisError] = useState(false);
   const [processing, setProcessing] = useState(false);
+  const [showHistory1, setShowHistory1] = useState<boolean>(true)
+  const [showHistory2, setShowHistory2] = useState<boolean>(false)
+  const [showHistory3, setShowHistory3] = useState<boolean>(false)
+
 
   const buy = useBuyNFT();
   const { isLoading } = useQuery("collection", {
@@ -69,6 +74,21 @@ function Explore() {
 
   async function picklistChange(e: React.ChangeEvent<HTMLSelectElement>) {
     const collectionName = e.target.value;
+    console.log(collectionName);
+    if (collectionName === "All Collections") {
+      setfiltered_Metadata(metadata);
+      return;
+    }
+    if (!marketItms || !metadata) return;
+    const [_marketItms, _metadata] = filterItems(
+      collectionName,
+      marketItms,
+      metadata
+    );
+    setfiltered_Metadata(_metadata);
+  }
+  async function displaycollection(collection_name: string) {
+    const collectionName = collection_name;
     console.log(collectionName);
     if (collectionName === "All Collections") {
       setfiltered_Metadata(metadata);
@@ -111,51 +131,133 @@ function Explore() {
       <p className="text-6xl font-bold text-center pb-14">
         Explore Collections
       </p>
-      <div className="flex mt-4 bg-black w-8/12 mb-8 mx-auto border border-secondary rounded overflow-hidden">
-        <span className="text-sm text-white px-4 py-2 bg-secondary whitespace-no-wrap">
-          Search:
-        </span>
-        <select
-          className=" py-2 w-full  bg-white"
-          onChange={(e) => {
-            picklistChange(e);
-          }}
-        >
-          <option>Cat Rescue NFTs</option>
-          {collectionList.map((collection, i) => (
-            <option key={i} value={collection.get("collectionAddress")}>
-              {collection.get("name")}
-            </option>
-          ))}
-        </select>
+      <div>
+        <Search />
       </div>
-      <hr />
-      {empty ? (
-        <div className="flex mx-auto justify-content-center mt-8">
-          <div className="mx-auto text-center">
-            <p className="text-4xl font-bold">
-              {" "}
-              There&apos;s currently no NFT on the Marketplace, come back later.
-            </p>
-          </div>
+
+      <div>
+        <div className="m-6 p-6 text-white bg-secondary  rounded-xl w-min md:w-[885px] min-h-[1200px]">
+          <ul className="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
+            <li className="mr-2">
+              <button
+                className={
+                  `inline-block p-4 rounded-t-lg bg-secondary ` +
+                  (showHistory1
+                    ? 'dark:hover:bg-gray-800 dark:hover:text-gray-300  '
+                    : 'active dark:bg-gray-800 dark:text-blue-500') +
+                  (!showHistory2
+                    ? 'dark:hover:bg-gray-800 dark:hover:text-gray-300  '
+                    : 'active dark:bg-gray-800 dark:text-blue-500') +
+                  (!showHistory3
+                    ? 'dark:hover:bg-gray-800 dark:hover:text-gray-300  '
+                    : 'active dark:bg-gray-800 dark:text-blue-500')
+                }
+                onClick={() => { setShowHistory1(true); setShowHistory2(false); setShowHistory3(false); displaycollection("First Collection"); }}
+              >
+                First Collection
+              </button>
+            </li>
+            <li className="mr-2">
+              <button
+                className={
+                  `inline-block p-4 rounded-t-lg ` +
+                  (!showHistory1
+                    ? 'dark:hover:bg-gray-800 dark:hover:text-gray-300  '
+                    : 'active dark:bg-gray-800 dark:text-blue-500') +
+                  (!showHistory2
+                    ? 'dark:hover:bg-gray-800 dark:hover:text-gray-300  '
+                    : 'active dark:bg-gray-800 dark:text-blue-500') +
+                  (!showHistory3
+                    ? 'dark:hover:bg-gray-800 dark:hover:text-gray-300  '
+                    : 'active dark:bg-gray-800 dark:text-blue-500')
+                }
+                onClick={() => { setShowHistory1(false); setShowHistory2(true); setShowHistory3(false); displaycollection("Second Collection"); }}
+              >
+                Second Collection
+              </button>
+            </li>
+            <li className="mr-2">
+              <button
+                className="inline-block p-4 rounded-t-lg "
+                onClick={() => { setShowHistory1(false); setShowHistory2(false); setShowHistory3(true); displaycollection("First Collection"); }}
+              >
+                Third  Collection
+              </button>
+            </li>
+            <li className="mr-2">
+              
+                <select className={
+                  "inline-block p-4 rounded-t-lg "
+                }
+                  
+                  placeholder="All"
+                  onChange={(e) => {
+                    picklistChange(e);
+                  }}
+                >
+                  <option>All collections</option>
+                  {collectionList.map((collection, i) => (
+                    <option key={i} value={collection.get("collectionAddress")}>
+                      {collection.get("name")}
+                    </option>
+                  ))}
+                </select>
+        
+            </li>
+          </ul>
         </div>
-      ) : (
-        <div className="md:flex justify-center">
-          <div className="px-4" style={{ maxWidth: "1600px" }}>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-              {filtered_metadata &&
-                filtered_metadata.map((nft, i) => (
-                  <NFTTile
-                    key={i}
-                    nft={nft}
-                    callback={handleBuy}
-                    button="Buy"
-                  />
-                ))}
+      </div>
+
+
+      {showHistory1 ? (
+        <>
+          <div className="md:flex justify-center">
+            <div className="px-4" style={{ maxWidth: "1600px" }}>
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+                {filtered_metadata &&
+                  filtered_metadata.map((nft, i) => (
+                    <NFTTile
+                      key={i}
+                      nft={nft}
+                      callback={handleBuy}
+                      button="Buy"
+                    />
+                  ))}
+              </div>
             </div>
           </div>
-        </div>
+        </>
+      ) : showHistory2 ? (
+        <>
+
+          {console.log("2")}
+        </>
+      ) : showHistory3 ? (
+        <>
+
+          {console.log("3")}
+        </>
+      ) : (
+        <>
+          <div className="md:flex justify-center">
+            <div className="px-4" style={{ maxWidth: "1600px" }}>
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+                {filtered_metadata &&
+                  filtered_metadata.map((nft, i) => (
+                    <NFTTile
+                      key={i}
+                      nft={nft}
+                      callback={handleBuy}
+                      button="Buy"
+                    />
+                  ))}
+              </div>
+            </div>
+          </div>
+          )
+        </>
       )}
+
       <Processing isOpen={processing} />
 
       {isSuccess && <ToastSucess isOpen={true} toggle={setIsSuccess} />}
@@ -163,5 +265,4 @@ function Explore() {
     </div>
   );
 }
-
 export default Explore;
